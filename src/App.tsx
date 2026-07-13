9a88c3a6901dde5856f7f15acd0a894ac2369db6
Pedro Toupitzen <pts.mdascruzes@gmail.com>
2026-07-13 16:02:32 -0300

    Initial commit - initialized project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        Task Lister
+      </section>
+    </>
+  )
+}
+
+export default App
