5f0cf3e5325306eec22010367937a37cdce8045b
Pedro Toupitzen <pts.mdascruzes@gmail.com>
2026-07-14 15:18:06 -0300

    Included different style for completed items

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,26 +5,27 @@ import InputCheckbox from './form/InputCheckbox'
 import type { Todo } from '../interfaces/todo';
 
 interface CardProps {
-	item: Todo
+	item: Todo,
+	key: string | null,
 }
 
-export default function Card({ item }: CardProps) {
+export default function Card({ item, key }: CardProps) {
 	const modalToggle = useModalStore((state) => state.modalToggle);
 	const loadForm = useTodoFormStore((state) => state.loadForm);
 	const deleteTodo = useTodoStore((state) => state.deleteTodo);
 	const toggleTodo = useTodoStore((state) => state.toggleTodo);
 	
-	const fntEditItem = () => {
+	const handleEditItem = () => {
 		loadForm(item);
 		modalToggle();
 	}
 
-	const fntDeleteItem = () => {
+	const handleDeleteItem = () => {
 		deleteTodo(item.id);
 	}
 
 	return (<div>
-		<div className="card-single">
+		<div className={`card-single ${item.completed ? 'card-completed' : ''}`} key={key}>
 			<div className="card-section">
 				<div className="card-title">{item.title}</div>
 				<div className="card-date">Created at {item.createdAt}</div>
@@ -44,8 +45,8 @@ export default function Card({ item }: CardProps) {
 					/>
 				</div>
 				<div className="flx-space-between">
-					<button onClick={fntEditItem} className={'btn'}>Edit</button>
-					<button onClick={fntDeleteItem} className={'btn btn-red'}>Delete</button>
+					<button onClick={handleEditItem} className={'btn'}>Edit</button>
+					<button onClick={handleDeleteItem} className={'btn btn-red'}>Delete</button>
 				</div>
 			</div>
 		</div>
